eca3e2165b895690a90d7bfc9e95356fe1135ef5
Abhay singh Bisht <abhaysinghbisht62@gmail.com>
2026-05-12 15:40:37 +0530

    Updated backend URL for deployment

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // When backend is deployed on Render, replace this with your Render API URL.
 
 (function () {
-  window.__API_BASE__ = "http://localhost:5000";
+  window.__API_BASE__ = "https://ai-campus-recovery-server.onrender.com";
 })();
